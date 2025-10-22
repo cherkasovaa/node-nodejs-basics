ff111e78125ed56b4540e0d3f581d05479fd1b1a
cherkasovaa <alina.ch1202@gmail.com>
2025-10-22 22:16:21 +0300

    feat: streams/transform solution

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+
+import { stdin, stdout } from 'node:process';
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-  // Write your code here
+  const reverseStream = new Transform({
+    transform(chunk, encoding, callback) {
+      this.push(chunk.toString().split('').reverse().join(''))
+      callback();
+    }
+  })
+
+  stdin.pipe(reverseStream).pipe(stdout);
 };
 
 await transform();
