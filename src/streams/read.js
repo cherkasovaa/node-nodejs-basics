2c145ea839eb02aaf278d801dadf3c608618bac2
cherkasovaa <alina.ch1202@gmail.com>
2025-10-22 21:19:03 +0300

    feat: streams/read solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+
+import { createReadStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { stdout } from 'node:process';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-  // Write your code here
+  const src = join(__dirname, 'files', 'fileToRead.txt');
+
+  let readableStream = createReadStream(src, 'utf8');
+  readableStream.pipe(stdout)
 };
 
 await read();
