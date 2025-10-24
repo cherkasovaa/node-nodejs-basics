3f5fdc696cefe41e76415ad0bd7e9b4dcca6e573
cherkasovaa <alina.ch1202@gmail.com>
2025-10-24 09:00:35 +0300

    feat: hash/calcHash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-  // Write your code here
+  const src = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+  const hash = createHash('sha256');
+  const stream = createReadStream(src);
+
+  stream.on('data', (chunk) => hash.update(chunk));
+  stream.on('end', () => {
+    console.log(hash.digest('hex'))
+  })
+
+  stream.on('error', (err) => {
+    console.error(err.message)
+  })
 };
 
 await calculateHash();
