95fa920ef58727572b7bc7cf1d79a0dcf8e7edf4
cherkasovaa <alina.ch1202@gmail.com>
2025-10-24 09:23:55 +0300

    feat: cli/env solution

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-  // Write your code here
+  const env = process.env;
+  
+  const result = Object.keys(env)
+    .filter((key) => key.startsWith('RSS_'))
+    .map((key) => `${key}=${env[key]}`);
+  
+  console.log(result.join('; '))
 };
 
 parseEnv();
